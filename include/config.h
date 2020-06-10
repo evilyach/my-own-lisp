5bfd2753f25c575bf2070becaceaa56f58135faf
Ilya Chesalin <evilyach@protonmail.com>
2020-06-10 20:09:52 +0300

    Added basic interactive prompt
    - Added a config header file with global vars;
    - Added a basic interactive prompt.
    
    See #3

---
+++ include/config.h
@@ -0,0 +1,6 @@
+/* Global variables */
+const char *MOL_VERSION = "0.1";
+
+/* Input console specific variables */
+#define INPUT_SIZE 2048
+static char input[INPUT_SIZE];
--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#include "config.h"
+
 int main(int argc, char **argv)
 {
-    printf("Hello World!\n");
+    printf("My Own Lisp v%s\n", MOL_VERSION);
+    printf("Press Ctrl+C to Exit\n\n");
+
+    while (1) {
+        fputs("my-own-lisp > ", stdout);
+        fgets(input, INPUT_SIZE, stdin);
+        printf("No, you are a %s", input);
+    }
 }
