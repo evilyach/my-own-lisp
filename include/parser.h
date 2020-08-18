cc6d0028e08dd12b67d01ae7933504c406dd4fa8
Ilya Chesalin <ilya.chesalin@evilya.ch>
2020-08-18 16:35:07 +0000

    Fix header resolving
    Before it caused a linker error like
    ```
    /usr/bin/ld: /tmp/cc8j1Ixp.o:(.data.rel.local+0x10): multiple definition of
    `MOL_LANGUAGE_RULES'; /tmp/ccqcOtKr.o:(.data.rel.local+0x0): first
    defined here
    collect2: error: ld returned 1 exit status
    ```

--- include/parser.h
+++ include/parser.h
@@ -1,8 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "../lib/mpc.h"
-
 const char *MOL_LANGUAGE_RULES = " \
     number     : /-?[0-9]+/ ; \
     operator   : '+' | '-' | '*' | '/' ; \
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../lib/mpc.h"
 #include "config.h"
 #include "edit.h"
 #include "parser.h"
